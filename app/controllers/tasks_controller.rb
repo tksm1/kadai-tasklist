315cd09d73783d06ccb88ab7d7a3e6f341715184
Ubuntu <ubuntu@ip-172-31-5-204.ap-northeast-1.compute.internal>
2021-10-04 09:46:45 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+
+    if @tasks.save
+      flash[:success] = 'タスクが正常に作成されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'タスクが作成されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+
+    if @tasks.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
+  end
+end
+
+private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
